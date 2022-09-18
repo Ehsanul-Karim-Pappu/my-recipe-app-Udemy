45a2291efe6911a0e208488208086abc22234f58
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-19 01:59:27 +0600

    Passing Data with Event and Property Binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -8,9 +8,16 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'This is a simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-  new Recipe('A Test Recipe', 'This is a simple test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is a simple test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe(
+      'Cheeseburger',
+      'A cheeseburger is a hamburger topped with cheese.',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg')
   ];
+  @Output() recipeItemClicked: EventEmitter<Recipe> = new EventEmitter();
 
   constructor() { }
 
