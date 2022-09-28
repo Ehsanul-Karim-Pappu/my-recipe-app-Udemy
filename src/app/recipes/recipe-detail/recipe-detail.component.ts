424f9aa274c516d99cfed8f1ba273a5a6d248224
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-28 20:12:01 +0600

    Added ingredients to Recipe details and pass ingredients from recipe details from shopping list with manage button

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from "../recipe.model";
+import {ShoppingListService} from "../../services/shopping-list.service";
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,9 +11,15 @@ import {Recipe} from "../recipe.model";
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe!: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
+  }
+
+  addToShoppingList(ingredients: Ingredient[]) {
+    for (const ingredient of ingredients) {
+      this.shoppingListService.ingredients.push(ingredient);
+    }
   }
 
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Recipe} from "../recipes/recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,21 @@ import {Recipe} from "../recipes/recipe.model";
 export class RecipeService {
   recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a simple test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
     new Recipe(
       'Cheeseburger',
       'A cheeseburger is a hamburger topped with cheese.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg')
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cheeseburger.jpg/1200px-Cheeseburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
   loadedFeature: string = 'recipe';
   recipeItemClicked!: Recipe;
